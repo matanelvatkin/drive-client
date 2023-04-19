6beed9fc3a6f5630ec2d3049bb28fdd92b6b18b4
Dael Molodic <Dael140@gmail.com>
2023-04-19 14:41:29 +0300

    finish dael

--- src/components/CurrentLocalPath/index.jsx
+++ src/components/CurrentLocalPath/index.jsx
@@ -1,4 +1,4 @@
-import { Breadcrumbs, Link, Typography } from '@mui/material'
+import { Breadcrumbs, Link, Typography, TextField } from '@mui/material'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import { setPath } from '../../features/document/documentSlice';
 import { useSelector, useDispatch } from 'react-redux'
@@ -8,20 +8,21 @@ const CurrentLocalPath = () => {
     const path = useSelector((state) => state.document.path)
     const dispatsh = useDispatch();
 
-    const HandelClick = (index) => {
+    const HandelClick = (e, index) => {
+        e.preventDefault();
         dispatsh(setPath(index));
     }
 
-    return (
-        <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNextIcon/>}>
-
-            {path.slice(0, path.length - 1).map((directory, i) => 
-                <Link key={directory} underline="hover" color="inherit" href="#" onClick={e => HandelClick(i)}>
-                    {directory} 
-                </Link>)
-            }
-            <Typography color="text.primary">{path[path.length - 1]}</Typography>
-        </Breadcrumbs>
+    return (<>
+            <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNextIcon/>}>
+                {path.slice(0, path.length - 1).map((directory, i) => 
+                    <Link key={directory} underline="hover" color="inherit" href="#" onClick={e => HandelClick(e, i)}>
+                        {directory} 
+                    </Link>)
+                }
+                <Typography color="text.primary">{path[path.length - 1]}</Typography>
+            </Breadcrumbs>
+        </>
     )
 }
 
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import CurrentLocalPath from '../CurrentLocalPath';
+import styles from "./style.module.css"
 
 
 export default function Main() {
 
-  return (
+  return ( <div className={styles.main}>
     <CurrentLocalPath />
+    <main>
+      main view
+    </main>
+  </div>
   )
 }
